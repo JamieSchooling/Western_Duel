2153b67271a0d1d6e11c4c0ee29e4348e6876636
EyeballSweat <eyeballsweatgaming@gmail.com>
2023-11-14 16:52:42 +0000

    Created Base Bar System
    Co-Authored-By: JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>

---
+++ Assets/Scripts/Bars/ShootSlider.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootSlider : MonoBehaviour
+{
+    [SerializeField] private Slider shootSlider;
+    [SerializeField] private float sliderShuffleDelay = 0.1f;
+    [SerializeField] private bool canMove;
+    [Range(0f, 1f)]
+    [SerializeField] private float validRange = 0.15f;
+    [SerializeField] Image fillLeft;
+    [SerializeField] Image fillRight;
+
+    private bool isMovingRight = true;
+
+    void Start()
+    {
+        //shootSlider.value = shootSlider.minValue;
+        Debug.Log(shootSlider.value);
+        StartCoroutine(SliderShuffle());
+    }
+
+
+    void Update()
+    {
+        fillLeft.fillAmount = validRange;
+        fillRight.fillAmount = validRange;
+
+        SliderSafetyNet();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (canMove)
+            {
+                canMove = false;
+                if (isMovingRight)
+                {
+                    Debug.Log(shootSlider.normalizedValue);
+                    Debug.Log(1 - shootSlider.normalizedValue <= validRange || shootSlider.normalizedValue <= validRange);
+                }
+
+            }
+            else
+            {
+                StartCoroutine(SliderShuffle());
+            }
+        }
+    }
+
+    private void SliderSafetyNet()
+    {
+        if (shootSlider.value < shootSlider.minValue)
+        {
+            shootSlider.value = shootSlider.minValue;
+        }
+        if (shootSlider.value > shootSlider.maxValue)
+        {
+            shootSlider.value = shootSlider.maxValue;
+        }
+    }
+
+    private IEnumerator SliderShuffle()
+    {
+        canMove = true;
+        while (canMove)
+        {
+            if (isMovingRight)
+            {
+                shootSlider.value++;
+            }
+            else
+            {
+                shootSlider.value--;
+            }
+
+            if (shootSlider.value >= shootSlider.maxValue)
+            {
+                isMovingRight = false;
+            }
+            else if (shootSlider.value <= shootSlider.minValue)
+            {
+                isMovingRight = true;
+            }
+            yield return new WaitForSeconds(sliderShuffleDelay);
+        }
+    }
+}
