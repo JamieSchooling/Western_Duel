3da990660afa58a8dfc25704d40a62adefcdae62
EyeballSweat <eyeballsweatgaming@gmail.com>
2023-11-15 13:23:45 +0000

    Random Factor + Revamped Positioning of Bars
    Co-Authored-By: JRRoddy <148975212+JRRoddy@users.noreply.github.com>

--- Assets/Scripts/Bars/ShootSlider.cs
+++ Assets/Scripts/Bars/ShootSlider.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.PlayerSettings;
 
 [RequireComponent(typeof(Slider))]
 public class ShootSlider : MonoBehaviour
@@ -19,8 +20,22 @@ public class ShootSlider : MonoBehaviour
     [SerializeField] private Color _colourFailArea = Color.red;
     [Range(1, 2)]
     [SerializeField] private int _playerNumber;
+    [SerializeField] private RectTransform RectLeft;
+    [SerializeField] private RectTransform RectRight;
+    [SerializeField] private RectTransform Indicate;
+    [SerializeField] RangeAttribute leftrange;
+    [SerializeField] RangeAttribute rightrange;
+    [SerializeField] private Vector3 Lmax;
+    [SerializeField] private Vector3 Lmin;
+    [SerializeField] private Vector3 Rmax;
+    [SerializeField] private Vector3 Rmin;
+    [SerializeField] private bool success;
+    [SerializeField] private int randomFactor;
 
     private Slider _shootSlider;
+
+    private float moveLeft;
+    private float moveRight;
     
     private bool _canMove;
     private bool _isMovingRight = true;
@@ -33,6 +48,8 @@ public class ShootSlider : MonoBehaviour
 
     void Start()
     {
+        MoveBars();
+
         if (_isMiddleSlider)
         {
             _fillLeft.color = _colourFailArea;
@@ -74,6 +91,8 @@ public class ShootSlider : MonoBehaviour
 
     void Update()
     {
+        MoveBars();
+
         _fillLeft.fillAmount = _validRange;
         _fillRight.fillAmount = _validRange;
 
@@ -84,17 +103,18 @@ public class ShootSlider : MonoBehaviour
             if (_canMove)
             {
                 _canMove = false;
-                bool isInRangeEdge = !_isMiddleSlider && (_shootSlider.normalizedValue <= _validRange || _shootSlider.normalizedValue >= 1 - _validRange);
-                bool isInRangeMiddle = _isMiddleSlider && _shootSlider.normalizedValue > _validRange && _shootSlider.normalizedValue < 1 - _validRange;
-                if (isInRangeEdge || isInRangeMiddle)
-                {
-                    Debug.Log("Success.");
-                    _shootBarEventChannel.InvokeOnSliderSuccess(_playerNumber);
-                }
-                else
-                {
-                    _shootBarEventChannel.InvokeOnSliderFail(_playerNumber);
-                }
+                ISin();
+                //bool isInRangeEdge = !_isMiddleSlider && (_shootSlider.normalizedValue <= _validRange || _shootSlider.normalizedValue >= 1 - _validRange);
+                //bool isInRangeMiddle = _isMiddleSlider && _shootSlider.normalizedValue > _validRange && _shootSlider.normalizedValue < 1 - _validRange;
+                //if (isInRangeEdge || isInRangeMiddle)
+                //{
+                //    Debug.Log("Success.");
+                //    _shootBarEventChannel.InvokeOnSliderSuccess(_playerNumber);
+                //}
+                //else
+                //{
+                //    _shootBarEventChannel.InvokeOnSliderFail(_playerNumber);
+                //}
 
             }
         }
@@ -136,6 +156,42 @@ public class ShootSlider : MonoBehaviour
                 _isMovingRight = true;
             }
             yield return new WaitForSeconds(_sliderShuffleDelay);
+        }
+    }
+
+    bool wasInvoked = false;
+
+    private void MoveBars()
+    {
+        moveRight = Mathf.RoundToInt(UnityEngine.Random.Range(0, randomFactor));
+        moveLeft = Mathf.RoundToInt(UnityEngine.Random.Range(0, randomFactor));
+
+        if (!wasInvoked)
+        {
+            wasInvoked = true;
+            RectLeft.anchoredPosition = new Vector2(RectLeft.anchoredPosition.x + moveRight, RectLeft.anchoredPosition.y);
+            RectRight.anchoredPosition = new Vector2(RectRight.anchoredPosition.x - moveLeft, RectRight.anchoredPosition.y);
+        }
+    }
+
+    private void ISin()
+    {
+        Lmax = RectTransformUtility.CalculateRelativeRectTransformBounds(_shootSlider.transform, RectLeft).max;
+        Lmin = RectTransformUtility.CalculateRelativeRectTransformBounds(_shootSlider.transform, RectLeft).min;
+        Rmax = RectTransformUtility.CalculateRelativeRectTransformBounds(_shootSlider.transform, RectRight).max;
+        Rmin = RectTransformUtility.CalculateRelativeRectTransformBounds(_shootSlider.transform, RectRight).min;
+
+        rightrange = new RangeAttribute(Rmin.x, Rmax.x);
+        leftrange = new RangeAttribute(Lmin.x, Lmax.x);
+        if (Indicate.transform.localPosition.x < leftrange.max && Indicate.transform.localPosition.x > leftrange.min || Indicate.transform.localPosition.x < rightrange.max && Indicate.transform.localPosition.x > rightrange.min)
+        {
+            Debug.Log("IS IN RANGE ");
+            _shootBarEventChannel.InvokeOnSliderSuccess(_playerNumber);
+
+        }
+        else
+        {
+            _shootBarEventChannel.InvokeOnSliderFail(_playerNumber);
         }
     }
 }
